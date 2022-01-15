c178b7ae5df71f9919b6ac99f01d38ec4dfd4e89
CecileGiang <cecilephuongthu.giang@gmail.com>
2022-01-15 14:23:31 +0100

    Add files via upload

--- dataloader.py
+++ dataloader.py
@@ -3,12 +3,13 @@
 ###############################################################################
 
 
-import numpy as np
-import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import random as rd
 
 
 ###############################################################################
@@ -23,7 +24,7 @@ movies_df = pd.read_csv("../data/ratings_Movies_and_TV.csv", names=["user", "ite
 class MoviesDataset(Dataset):
     """ Constucteur du dataset pour rating_Movies_and_TV.csv.
     """
-    def __init__(self, data, min_length = 1000):
+    def __init__(self, data, min_length = 100, ratio_neg = 0.5):
         """ Constructeur de la classe MoviesDataset.
             @param data: données ratings_Movies_and_TV.csv sous la forme d'un dataframe pandas
             @param min_length: int, longueur minimale de l'historique des utilisateurs
@@ -61,21 +62,32 @@ class MoviesDataset(Dataset):
         
         self.data = self.data.groupby('user').agg({'item' : list, 'timestamp' : list}).reset_index()
         
+        self.dataset = self.data.copy()
+        self.dataset["label"] = 1
+        
+        self.data_neg = self.data.sample(frac = ratio_neg)
+        
+        for i, d in self.data_neg.iterrows():
+            self.dataset = self.dataset.append({"user" : d[0], "item" : d[1], "timestamp" : d[2], "label" : 0} , ignore_index=True)
+            item_list = list(self.item_map.values())
+            item_list.remove(d[1][-1])
+            d[1][-1] = rd.choice(item_list)
+        
     def __len__(self):
         """ Retourne la taille du dataset.
         """
-        return len(self.data)
+        return len(self.dataset)
     
     def __getitem__(self, i):
         """ Retourne l'échantillon à la position i.
         """
-        return torch.tensor( self.data.iloc[i]['item'][:-1] ), torch.tensor( self.data.iloc[i]['timestamp'][:-1] ), torch.tensor( self.data.iloc[i]['item'][-1] )
+        return torch.tensor( self.dataset.iloc[i]['item'][:-1] ), torch.tensor( self.dataset.iloc[i]['timestamp'][:-1] ), torch.tensor( self.dataset.iloc[i]['item'][-1] ), torch.tensor( self.dataset.iloc[i]['label'] )
 
 
 def pad_collate(batch):
     """ Padding pour les séquences utilisateurs.
     """
-    (xx, tt, yy) = zip(*batch)
+    (xx, tt, yy, ll) = zip(*batch)
     
     # Padding pour les séquences d'items et les séquences de temps
     xx_pad = pad_sequence(xx, batch_first=True, padding_value=0)
@@ -88,4 +100,4 @@ def pad_collate(batch):
         mask = np.where(pp_pad[i], 1, 0)
         pp_pad[i] = np.arange(1, len(pp_pad[i]) + 1) * mask
     
-    return xx_pad, tt_pad, torch.tensor(pp_pad), torch.tensor(yy)
+    return xx_pad, tt_pad, torch.tensor(pp_pad), torch.tensor(yy), torch.tensor(ll)
